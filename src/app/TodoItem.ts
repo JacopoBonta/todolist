a755e4fbffcf69a08dfe0ad0683d09247073d174
jacopobonta <bonta.jacopo@gmail.com>
2018-06-12 16:30:15 +0200

    fix missing new line at the end of the file

--- src/app/TodoItem.ts
+++ src/app/TodoItem.ts
@@ -1,11 +1,11 @@
 export class TodoItem {
-    creationDate: Date
-    isCompleted: boolean
+  creationDate: Date
+  isCompleted: boolean
 
-    constructor(public id: number,
-                public title: string,
-                public description: string) {
-        this.creationDate = new Date()
-        this.isCompleted = false
-    }
+  constructor(public id: number,
+            public title: string,
+            public description: string) {
+    this.creationDate = new Date()
+    this.isCompleted = false
+  }
 }
